375c74b28b8ec2b5f591704da2daa025cb040c2f
emanoj1 <manojventures@gmail.com>
2024-03-16 09:56:06 +1100

    Set env for JWT

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,5 @@
+#flask related environment variables
 FLASK_APP=todo
 FLASK_DEBUG=1
 FLASK_ENV=development
+#FLASK_RUN_PORT=8080
--- todo.py
+++ todo.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from models import db
 from blueprints import todo_blueprint
@@ -6,12 +7,15 @@ from flask_jwt_extended import JWTManager
 from datetime import timedelta
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://todo_dev:12345@localhost:5432/todo_db"
+
+#configs
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['JWT_SECRET_KEY'] = 'jwt_secret_key'  # Change this to a secure secret key
+app.config['JWT_SECRET_KEY']=os.environ.get("JWT_SECRET_KEY")
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)  # Example expiration time for access tokens
 
+#connect libraries with flask app
 db.init_app(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
