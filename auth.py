e53150b05ccd830676e576220274365bee7feeb3
emanoj1 <manojventures@gmail.com>
2024-03-16 16:46:45 +1100

    Define User signup and authentication

--- auth.py
+++ auth.py
@@ -1,8 +1,34 @@
 from flask import Blueprint, jsonify, request
-from models import User
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_bcrypt import generate_password_hash
+from models import db, User
 
 auth_blueprint = Blueprint('auth', __name__)
+
+#Define Signup route
+@auth_blueprint.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+# Check if username exists
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        return jsonify({'error': 'Username already exists'}), 409
+    
+    # Create a new user
+    new_user = User(username=username, password_hash=generate_password_hash(password))
+
+    # Add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    # Generate an access token for the new user
+    access_token = create_access_token(identity=new_user.id)
+
+    # Return a response with the access token
+    return jsonify({'access_token': access_token}), 201
 
 @auth_blueprint.route('/login', methods=['POST'])
 def login():
