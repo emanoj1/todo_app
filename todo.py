92854dcb778c4c06e76828daf1be574ccdbbad66
emanoj1 <manojventures@gmail.com>
2024-03-16 12:28:30 +1100

    Define create_app

--- todo.py
+++ todo.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from models import db
 from blueprints import todo_blueprint
 from flask_migrate import Migrate
@@ -8,16 +9,18 @@ from datetime import timedelta
 
 app = Flask(__name__)
 
-#configs
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get("DATABASE_URI")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['JWT_SECRET_KEY']=os.environ.get("JWT_SECRET_KEY")
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)  # Example expiration time for access tokens
+def create_app():
+    app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get("DATABASE_URI")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['JWT_SECRET_KEY']=os.environ.get("JWT_SECRET_KEY")
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)  # Example expiration time for access tokens
 
 #connect libraries with flask app
-db.init_app(app)
-migrate = Migrate(app, db)
-jwt = JWTManager(app)
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+    jwt = JWTManager(app)
 
-app.register_blueprint(todo_blueprint)
+    app.register_blueprint(todo_blueprint)
+
+    return app
